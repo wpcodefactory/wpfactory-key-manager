805e662bcde94fbbb78850244b397ca8757f218a
Pablo Pacheco <pablo.sg.pacheco@gmail.com>
2024-09-19 18:30:51 -0300

    Update gitignore

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'wpfactory/wpfactory-key-manager',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => 'fe040496ee306c01ff68f989faa85dc5aae8a80d',
+        'reference' => '32519997d45df4f4e6f0cc4caaca9724456a0a7b',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -11,20 +11,18 @@
     ),
     'versions' => array(
         'wpfactory/wpfactory-admin-menu' => array(
-            'pretty_version' => 'dev-master',
-            'version' => 'dev-master',
+            'pretty_version' => 'v1.0.0',
+            'version' => '1.0.0.0',
             'reference' => '7fe327fecf08fc5a14c3be54ad94a8a7006b4a08',
             'type' => 'library',
             'install_path' => __DIR__ . '/../wpfactory/wpfactory-admin-menu',
-            'aliases' => array(
-                0 => '9999999-dev',
-            ),
+            'aliases' => array(),
             'dev_requirement' => false,
         ),
         'wpfactory/wpfactory-key-manager' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => 'fe040496ee306c01ff68f989faa85dc5aae8a80d',
+            'reference' => '32519997d45df4f4e6f0cc4caaca9724456a0a7b',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
